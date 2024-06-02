6b2830da1986bac90dd99f66945f8c4c60a73bc8
yuki <yuki.be.free.fk@gmail.com>
2024-06-02 20:26:38 +0900

    位置情報のPermission実装

--- app/src/main/java/befree/ramen_eating_and_walking/MainActivity.kt
+++ app/src/main/java/befree/ramen_eating_and_walking/MainActivity.kt
@@ -1,20 +1,37 @@
 package befree.ramen_eating_and_walking
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import befree.ramen_eating_and_walking.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
+import befree.ramen_eating_and_walking.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
+import befree.ramen_eating_and_walking.PermissionUtils.isPermissionGranted
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-
-class MainActivity : AppCompatActivity () {
+class MainActivity : AppCompatActivity (),
+    ActivityCompat.OnRequestPermissionsResultCallback {
 
     private lateinit var binding: ActivityMainBinding
+
     // 位置情報を取得するためのプロパティ宣言
+    private lateinit var mMap: GoogleMap
+    private var permissionDenied = false
 
     // Bottom Navigationが選択された時の設定
     private val onItemSelectedListener = NavigationBarView.OnItemSelectedListener { item ->
@@ -50,8 +67,6 @@ class MainActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -62,10 +77,13 @@ class MainActivity : AppCompatActivity () {
         binding.navView.setOnItemSelectedListener(onItemSelectedListener)
 
         // 初期表示
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayout, HomeFragment())
-            .commit()
+        val homeFragment = supportFragmentManager.beginTransaction()
+        homeFragment.replace(R.id.frameLayout, HomeFragment())
+        homeFragment.commit()
         title = getString (R.string.menu_home)
+
+        // 位置情報を取得する許可をとる
+        enableMyLocation()
 
     }
 
@@ -80,6 +98,7 @@ class MainActivity : AppCompatActivity () {
         else { // 未ログインの場合の処理
             binding.toolbar.visibility = View.INVISIBLE
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -100,4 +119,108 @@ class MainActivity : AppCompatActivity () {
 
         return super.onOptionsItemSelected(item)
     }
+
+    /**
+     * ACCESS_FINE_LOCATIONが許可されている場合
+     * より正確な位置情報を利用可能にする
+     */
+    private fun enableMyLocation() {
+
+        // 権限が付与されているか確認し、付与されている場合はマイロケーションレイヤーを有効にする。
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        // 許可根拠ダイアログを表示するかどうか
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            PermissionUtils.RationaleDialog.newInstance(
+                LOCATION_PERMISSION_REQUEST_CODE, true
+            ).show(supportFragmentManager, "dialog")
+            return
+        }
+
+        // そうでない場合は、許可を求める。
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            // 許可されている場合は、マイロケーションレイヤーを有効にする
+            enableMyLocation()
+        } else {
+            // エラーメッセージを表示する
+            // フラグメントの再開時にパーミッション不足エラーダイアログを表示する。
+            permissionDenied = true
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+
+    /**
+     * 位置情報の取得権限がないことをダイアログで表示する。
+     */
+    private fun showMissingPermissionError() {
+        newInstance(true).show(supportFragmentManager, "dialog")
+    }
+
+    companion object {
+        /**
+         * Request code for location permission request.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        const val LOCATION_PERMISSION_REQUEST_CODE = 300
+    }
+
 }
--- app/src/main/java/befree/ramen_eating_and_walking/MapFragment.kt
+++ app/src/main/java/befree/ramen_eating_and_walking/MapFragment.kt
@@ -1,22 +1,33 @@
 package befree.ramen_eating_and_walking
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import befree.ramen_eating_and_walking.databinding.FragmentMapBinding
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment: Fragment(), OnMapReadyCallback {
+class MapFragment: Fragment(),
+    OnMapReadyCallback,
+    OnMyLocationButtonClickListener,
+    OnMyLocationClickListener {
+
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
     // Google Map APIを使うためのプロパティ
     private lateinit var mMap: GoogleMap
+    private var permissionMyLocation = false
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,20 +40,27 @@ class MapFragment: Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // Google Mapを描画
         val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        mapFragment?.getMapAsync (this)
     }
 
+    // Mapの設定
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        checkPermissionMyLocation()
         // 代表地点をセット
-        val tokyo = LatLng(35.39291572, 139.44288869)
+        val tokyo = LatLng(35.681236, 139.767125)
         // ズームレベルをセット
         val zoomLevel = 13.0f
 
         mMap.apply {
             // マーカーをセット
-            addMarker(MarkerOptions().position(tokyo).title("Marker in tokyo"))
+            addMarker(
+                MarkerOptions()
+                    .position(tokyo)
+                    .title("店舗名")
+                    .snippet("住所")
+            )
+            // .icon()) アイコンを設定
             // カメラを代表地点を中心にしてzoomする
             moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, zoomLevel))
 
@@ -54,9 +72,50 @@ class MapFragment: Fragment(), OnMapReadyCallback {
                 isScrollGesturesEnabled = true
                 // ピンチイン・アウトで縮尺を変更可能にする
                 isZoomGesturesEnabled = true
-                // 現在地ボタン表示
-                isMyLocationButtonEnabled = true
+            }
+
+            // 位置情報取得が許可されている場合の処理
+            if (permissionMyLocation) {
+                // 現在地ボタンの表示
+                isMyLocationEnabled
+                setOnMyLocationClickListener {
+                    onMyLocationClick(it)
+                }
+                setOnMyLocationButtonClickListener {
+                    onMyLocationButtonClick()
+                }
+
+                // 現在地にマップを移動
             }
         }
+    }
+
+    private fun checkPermissionMyLocation () {
+        // 権限が付与されているか確認し、付与されている場合はマイロケーションレイヤーを有効にする。
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            permissionMyLocation = true
+            return
+        }
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(requireContext(), "${getString(R.string.my_location)}\n$location", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(requireContext(), getString(R.string.move_my_location), Toast.LENGTH_SHORT)
+            .show()
+        // falseを返すことで、イベントを消費せず、デフォルトの動作を維持する。
+        // (カメラはユーザーの現在位置に合わせてアニメーションする）。
+        return false
     }
 }
